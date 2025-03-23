601ae7a8e136d743ecf69f154085ab33b086a1c3
rentschlerm <dboyclayton@gmail.com>
2025-03-23 15:51:17 +0800

    ..

--- config.php
+++ config.php
@@ -12,19 +12,3 @@ try {
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-?>
-
-
-<!-- <?php
-// $host = 'localhost';
-// $dbname = 'senior_connect';
-// $username = 'root';
-// $password = '';
-
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     die("Connection failed: " . $e->getMessage());
-// }
-?> -->
