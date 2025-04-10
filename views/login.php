15fe2a51a5890afbdede71ef3947260a858090ae
rentschlerm <dboyclayton@gmail.com>
2025-04-10 11:55:41 +0800

    6.1

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@ require '../vendor/autoload.php';
 require_once 'authFunctions.php';
 
 $client = new Google\Client();
-$client->setAuthConfig(__DIR__ . '/../auth/client_secret_573751304329-u46e5l3l4o001omab337gl4e9jbsh8a8.apps.googleusercontent.com.json');
+$client->setAuthConfig(__DIR__ . '/../auth/client_secret.json');
 $client->setRedirectUri('https://senior-production-f9d8.up.railway.app/auth/oauth-callback.php');
 $client->addScope('email');
 $client->addScope('profile');
