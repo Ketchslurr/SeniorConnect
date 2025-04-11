9fcc89eddac5ee2784418da5685485d9479e1b2e
rentschlerm <dboyclayton@gmail.com>
2025-04-11 10:44:42 +0800

    6.5.3 = gana na tawn

--- auth/config.php
+++ auth/config.php
@@ -4,7 +4,7 @@ require '../vendor/autoload.php';
 session_start();
 
 $client = new Google\Client();
-$client->setAuthConfig(__DIR__ . '/client_secret.json');
+$client->setAuthConfig(__DIR__ . '/client_secret_573751304329-4hia4bhoqa6vt3uk8of75ps7eqep5tka.apps.googleusercontent.com.json');
 $client->setRedirectUri('https://senior-production-f9d8.up.railway.app/auth/oauth-callback.php');
 $client->addScope(Google\Service\Calendar::CALENDAR);
 $client->setAccessType('offline'); // To get a refresh token
